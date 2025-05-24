409f0404100ec799b57269aef5c2bf833107054d
ofboox <mateuszksiazek44@gmail.com>
2025-05-24 02:39:14 +0200

    created register page

--- client/src/views/loginView.js
+++ client/src/views/loginView.js
@@ -1,27 +1,32 @@
-import {login} from "../events/authorization"
+import {login} from "../events/authorization";
+import { useState } from 'react';
 
-function loginView() {
-    return(
+function LoginView(props) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
         <>
             <table>
                 <tbody>
                     <tr>
                         <td>Username:</td>
-                        <td><input id="username" type="text" /></td>
+                        <td><input id="username" type="text" onChange={(event) => setUsername(event.target.value)}/></td>
                     </tr>
                     <tr>
                         <td>Password:</td>
-                        <td><input id="password" type="password" /></td>
+                        <td><input id="password" type="password" onChange={(event) => setPassword(event.target.value)}/></td>
                     </tr>
                 </tbody>
             </table>
-            <button onClick={()=> {
-                const username = document.querySelector("#username").value;
-                const password = document.querySelector("#password").value;
-                login(username, password);
+            <button onClick={() => {
+               login(username, password);
             }}>Login</button>
-        </>
-    )
+	   <div>
+	   	<a onClick={props.setRegisterPage}>Rejestracja</a>
+       	   </div>
+	</>
+    );
 }
 
-export default loginView
+export default LoginView;
